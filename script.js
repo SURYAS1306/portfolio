4befa7e7f7704b5b32be0be280f8ff69cb0fe67a
Surya Srinivasan <suryasrinivasan1306@gmail.com>
2026-03-09 02:51:31 +0530

    theme using js

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleTheme(){
+
+let body=document.body;
+
+if(body.style.backgroundColor==="white"){
+    body.style.backgroundColor="#0f172a";
+    body.style.color="white";
+}
+else{
+    body.style.backgroundColor="white";
+    body.style.color="black";
+}
+
+}
